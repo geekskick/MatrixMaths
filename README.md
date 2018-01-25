c8193c739f15b71651254943ae03cb8e5e490df6
geekskick <geeks_kick@yahoo.co.uk>
2018-01-25 00:35:59 +0000

    Examples added to the main.cpp and the README.md. error fixed in .gitignore file

--- README.md
+++ README.md
@@ -12,8 +12,52 @@ The matrix library allows for some matrix operations to be performed.
 * Find the Determinant
 * Find the cofactors
 * Add a row or column
+* Stream the matrix to stdout
 
 I used the library when I didn't have access to MATLAB and didn't want to do matrix operations by hand.
+
+## Usage
+
+```c++
+// Make a 2x2 identity matrix.
+matrix m = matrix::identity(2);
+
+// Add a row to the matrix
+std::vector<double> coeffs = { 0.5, 4 };
+m.add_row(coeffs);
+
+// Add a column to the matrix
+// Note the matrix is currently a 3x2, so the column needs to have 3 items.
+coeffs.emplace_back(8.0);
+m.add_col(coeffs);
+
+// Transpose the matrix
+// Note the .transpose() method is const, it doesn't alter the contents of the matrix.
+m = m.transpose();
+
+// Find the determinant
+// Note the .determinant() method is const, it doesn't alter the contents of the matrix.
+const double det = m.determinant();
+
+std::cout << m << "Determinant is " << det << std::endl;
+```
+
+### Exceptions
+
+If you try to do something which is illegal a `matrix_exception` will be thrown. Using it's `.what()` method will give a clue as to what was illegal.
+
+Example:
+```c++
+matrix m = matrix::identity(2);
+try{
+    m += matrix::identity(4);
+}
+catch(matrix_exception &e){
+    std::cerr << e.what() << std::endl;
+}
+```
+
+Gives the output: `Matrix Exception: Dimensions not compatible for * operator [2 vs. 4]`.
 
 ## Build
 There are no dependancies.
@@ -23,12 +67,14 @@ There are no dependancies.
 > cmake .. 
 ```
 
-## Use
+## Linking
+
 The matrix library is built into the `build/src` directory, along with it's interface in `matrix.hpp`. This is linked using cmake's `target_include_directories(matrix PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})` command, so when you build your application using the command `target_link_libraries(MyApp LINK_PUBLIC matrix)` and the header file will be added to your application's include path.
 
 ## Test
+
 Run tests from the build directory using 
 ```
 ./test/matrix_test
 ```
-
+The tests are run using the `catch` tool's header: `catch.hpp`.
--- main.cpp
+++ main.cpp
@@ -8,102 +8,34 @@
 
 int main(int argc, const char * argv[]) {
 	
-	matrix m = matrix::identity(4);
+	matrix m = matrix::identity(2);
 
-	std::cout << "Hello, World!\n";
-	std::cout << m 		<< std::endl;
-	std::cout << m * 4 	<< std::endl;
-	
-	matrix a({{0,1},{-8,-6}});
-	matrix b(1,2);
-	b[0] = {0};
-	b[1] = {1};
-	std::cout << "A*B = " << std::endl << a * b << std::endl;
-	
-	matrix lhs(2,2);
-	lhs[0] = {1, 2};
-	lhs[1] = {3, 4};
-	std::cout << "lhs:" 		<< std::endl << lhs 				<< std::endl;
-	std::cout << "det(lhs):" 	<< std::endl << lhs.determinant() 	<< std::endl << std::endl;
-	std::cout << "adj(lhs):" 	<< std::endl << lhs.adj() 			<< std::endl;
-	std::cout << "lhsT:" 		<< std::endl << lhs.transpose() 	<< std::endl;
-	try{
-		std::cout << "lhs^-1:" 	<< std::endl << lhs.inverse() 	<< std::endl;
-	}
-	catch (matrix_exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	
-	matrix n(3,3);
-	n[0] = {1,2,3};
-	n[1] = {4,5,6};
-	n[2] = {7,8,9};
-	std::cout << "n:" 		<< std::endl << n 				<< std::endl;
-	std::cout << "det(n):" 	<< std::endl << n.determinant() 	<< std::endl << std::endl;
-	std::cout << "adj(n):" 	<< std::endl << n.adj() 		<< std::endl;
-	std::cout << "nT:" 		<< std::endl << n.transpose() 	<< std::endl;
-	std::cout << "nT * I"	<< std::endl << n.transpose() * matrix::identity(n.rows()) << std::endl;
-	try{
-		std::cout << "n^-1:" << std::endl << n.inverse() 	<< std::endl;
-	}
-	catch (matrix_exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	
-	try{
-		std::cout << "m * n:" << std::endl << m * n << std::endl;
-	}
-	catch (matrix_exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	std::vector<double> n_row = { 0, 1, 2, 3 };
-	std::vector<double> n_col = { 0, 1 ,2, 3, 4 };
-	
-	m.add_row(n_row);
-	std::cout << m << std::endl;
+	// Add a row to the matrix
+	std::vector<double> coeffs = { 0.5, 4 };
+	m.add_row(coeffs);
 
-	m.add_col(n_col);
-	std::cout << m << std::endl;
+	// Add a column to the matrix
+	// Note the matrix is currently a 3x2, so the column needs to have 3 items.
+	coeffs.emplace_back(8.0);
+	m.add_col(coeffs);
+
+	// Transpose the matrix
+	// Note the .transpose() method is const, it doesn't alter the contents of the matrix.
+	m = m.transpose();
+
+	// Find the determinant
+	// Note the .determinant() method is const, it doesn't alter the contents of the matrix.
+	const double det = m.determinant();
+
+	std::cout << m << "Determinant is " << det << std::endl;
+
+	m = matrix::identity(2);
 	
 	try{
-		std::cout << "Adding to m:" << std::endl << m.add_row(n_row) << std::endl;
-	}
-	catch(matrix_exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	
-	matrix l(3,1);
-	l[0] = { 1, 2, 3 };
-	
-	matrix r(4,3);
-	r[0] = { 4 ,3, 2, 1 };
-	r[1] = { 5, 4, 3, 2 };
-	r[2] = { 6, 5, 4, 3 };
-	
-	try{
-		std::cout << "r * l:" << std::endl << r * l << std::endl;
-	}
-	catch(matrix_exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		std::cout << "l * r:" << std::endl << l * r << std::endl;
-	}
-	catch(matrix_exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	
-	matrix in_place({{4,3},{2,1}});
-	std::cout << "in_place:" 		<< std::endl << in_place 				<< std::endl;
-	std::cout << "det(in_place):" 	<< std::endl << in_place.determinant() 	<< std::endl << std::endl;
-	std::cout << "adj(in_place):" 	<< std::endl << in_place.adj() 			<< std::endl;
-	std::cout << "in_placeT:" 		<< std::endl << in_place.transpose() 	<< std::endl;
-	std::cout << "in_placeT * I"	<< std::endl << in_place.transpose() * matrix::identity(in_place.rows()) << std::endl;
-	try{
-		std::cout << "in_place^-1:" << std::endl << in_place.inverse() 		<< std::endl;
+		m *= matrix::identity(4);
 	}
 	catch (matrix_exception &e){
-		std::cout << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
 	}
 	return 0;
 }
