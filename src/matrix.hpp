3b480828327a37066a8033e465e40c68a6cf575b
geekskick <geeks_kick@yahoo.co.uk>
2019-05-19 20:11:00 +0100

    remedied clang format checks

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -14,8 +14,8 @@
 
 class matrix_exception : public std::exception {
 public:
-    matrix_exception(const std::string msg) : m_msg(msg) {}
-    const char *what(void) const throw() { return (std::string("Matrix Exception: ") + m_msg).c_str(); }
+    explicit matrix_exception(const std::string &msg) : m_msg{msg} {}
+    const char *what() const noexcept override { return (std::string("Matrix Exception: ") + m_msg).c_str(); }
 
 private:
     const std::string m_msg;
@@ -23,16 +23,16 @@ private:
 
 template <typename T> class matrix {
 public:
-    matrix(void) : m_cdimension(0), m_rdimension(0), m_values(0, std::vector<T>(0, 0.0)) {}
+    matrix() : m_cdimension{0}, m_rdimension{0}, m_values(0, std::vector<T>(0, T{})) {}
 
-    matrix(const int row_dimension, const int col_dimension)
-        : m_cdimension(col_dimension), m_rdimension(row_dimension), m_values(m_rdimension, std::vector<T>(m_cdimension, 0.0)) {}
+    matrix(const std::size_t row_dimension, const std::size_t col_dimension)
+        : m_cdimension{std::move(col_dimension)}, m_rdimension{std::move(row_dimension)}, m_values(m_rdimension, std::vector<T>(m_cdimension, T{})) {}
 
-    matrix(const std::vector<std::vector<T>> &values) : m_cdimension((int)values[0].size()), m_rdimension((int)values.size()), m_values(values) {}
+    explicit matrix(const std::vector<std::vector<T>> &values) : m_cdimension{values[0].size()}, m_rdimension{values.size()}, m_values{values} {}
 
     ~matrix() {}
 
-    static matrix identity(const int dimension) {
+    static matrix identity(const std::size_t dimension) {
         matrix result(dimension, dimension);
         for (int i = 0; i < dimension; i++) {
             result[i][i] = 1;
@@ -40,7 +40,7 @@ public:
         return result;
     }
 
-    void clear(void) { fill(0); }
+    void clear() { fill(0); }
 
     void fill(const T value) {
         for (int r = 0; r < m_rdimension; r++) {
@@ -72,18 +72,18 @@ public:
         return *this;
     }
 
-    double determinant(void) const {
+    double determinant() const {
         if (m_cdimension != m_rdimension) {
             throw matrix_exception("Rows and Columns must be equal to get the determinant");
         }
         return recur_determinant(*this);
     }
 
-    int rows(void) const { return m_rdimension; }
+    std::size_t rows() const { return m_rdimension; }
 
-    int cols(void) const { return m_cdimension; }
+    std::size_t cols() const { return m_cdimension; }
 
-    std::vector<T> column(const int c) const {
+    std::vector<T> column(const std::size_t c) const {
         std::vector<T> col;
         for (int r = 0; r < rows(); r++) {
             col.push_back(m_values[r][c]);
@@ -91,7 +91,7 @@ public:
         return col;
     }
 
-    matrix transpose(void) const {
+    matrix transpose() const {
         matrix result(m_cdimension, m_rdimension);
         for (int r = 0; r < m_rdimension; r++) {
             for (int c = 0; c < m_cdimension; c++) {
@@ -101,7 +101,7 @@ public:
         return result;
     }
 
-    matrix inverse(void) const {
+    matrix inverse() const {
         const double det = determinant();
         if (det == 0.0) {
             throw matrix_exception("Unable to invert as the determinant is 0");
@@ -109,29 +109,27 @@ public:
         return (adj() / det);
     }
 
-    matrix adj(void) const {
+    matrix adj() const {
         if (m_cdimension == 2 && m_rdimension == 2) {
             matrix result = *this;
             result[0][0] = m_values[1][1];
             result[1][1] = m_values[0][0];
-            // std::cout << result.cofactors();
             return result.cofactors();
         }
         return minors().cofactors().transpose();
     }
 
-    matrix cofactors(void) const {
+    matrix cofactors() const {
         matrix result = *this;
         for (int r = 1; r <= m_rdimension; r++) {
             for (int c = r % 2; c < m_cdimension; c += 2) {
-                // std::cout << "[" << r-1 << ", " << c << "]" << std::endl;
                 result.m_values[r - 1][c] *= -1;
             }
         }
         return result;
     }
 
-    matrix minors(void) const {
+    matrix minors() const {
         matrix result(m_cdimension, m_rdimension);
         for (int c = 0; c < m_cdimension; c++) {
             for (int r = 0; r < m_rdimension; r++) {
@@ -140,11 +138,9 @@ public:
                 // populate determinant matrix
                 for (int ic = 0; ic < m_cdimension - 1; ic++) {
                     for (int ir = 0; ir < m_rdimension - 1; ir++) {
-                        // std::cout << "[" << (ir >= r ? ir + 1 : ir) << ", " << (ic >= c ? ic + 1 : ic) << "]" <<std::endl;
                         temp[ir][ic] = m_values[ir >= r ? ir + 1 : ir][ic >= c ? ic + 1 : ic];
                     }
                 }
-                // std::cout << "matrix of minors calculatation: " << temp << std::endl;
                 result[r][c] = temp.determinant();
             }
         }
@@ -294,8 +290,8 @@ public:
     }
 
     bool operator!=(const matrix &rhs) const { return !(rhs == *this); }
-    std::vector<T> &operator[](const int x) { return m_values[x]; }
-    const std::vector<T> &operator[](const int x) const { return m_values[x]; }
+    std::vector<T> &operator[](const std::size_t x) { return m_values[x]; }
+    const std::vector<T> &operator[](const std::size_t x) const { return m_values[x]; }
 
     friend std::ostream &operator<<(std::ostream &os, const matrix &mat) {
         for (auto row : mat.m_values) {
@@ -309,8 +305,8 @@ public:
     }
 
 protected:
-    int m_cdimension;
-    int m_rdimension;
+    std::size_t m_cdimension{};
+    std::size_t m_rdimension{};
     std::vector<std::vector<T>> m_values;
 
     double recur_determinant(matrix sub_matrix) const {
@@ -327,7 +323,6 @@ protected:
                     temp[r][c] = sub_matrix[upper_r][upper_c];
                 }
             }
-            // std::cout << "Determinant temp" << std::endl << sub_matrix[0][s] << "x" << temp << std::endl;
             if (s % 2 == 0) {
                 sum += recur_determinant(temp) * sub_matrix[0][s];
             } else {
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,8 @@
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
 #include "matrix.hpp"
+#include <catch2/catch.hpp>
 
-TEST_CASE("Different types"){
+TEST_CASE("Different types") {
     using dm = matrix<double>;
     const auto dminst = dm();
     using im = matrix<int>;
@@ -14,258 +14,244 @@ TEST_CASE("Different types"){
     using bm = matrix<bool>;
     const auto bminst = bm();
 }
-TEST_CASE("Creating Matrices"){
+TEST_CASE("Creating Matrices") {
 
-	matrix<double> m;
-	REQUIRE(m.rows() == 0);
-	REQUIRE(m.cols() == 0);
-	matrix<double> p(7,9);
-	REQUIRE(p.rows() == 7);
-	REQUIRE(p.cols() == 9);
+    matrix<double> m;
+    REQUIRE(m.rows() == 0);
+    REQUIRE(m.cols() == 0);
+    matrix<double> p(7, 9);
+    REQUIRE(p.rows() == 7);
+    REQUIRE(p.cols() == 9);
 
-	for (int i = 0; i < 7; ++i)
-	{
-		REQUIRE(p[i] == std::vector<double>(9,0));
-	}
-	for (int i = 0; i < 9; ++i)
-	{
-		REQUIRE(p.column(i) == std::vector<double>(7,0));
-	}
+    for (int i = 0; i < 7; ++i) {
+        REQUIRE(p[i] == std::vector<double>(9, 0));
+    }
+    for (int i = 0; i < 9; ++i) {
+        REQUIRE(p.column(i) == std::vector<double>(7, 0));
+    }
 }
 
-TEST_CASE("Assigning Values"){
-	auto m = matrix<double>::identity(6);
+TEST_CASE("Assigning Values") {
+    auto m = matrix<double>::identity(6);
 
-	for(int i = 0; i < 6; i++){
-		for(int c = 0; c < 6; c++){
-			REQUIRE(m[i][c] == (i == c? 1.0: 0.0));
-		}
-	}
+    for (int i = 0; i < 6; i++) {
+        for (int c = 0; c < 6; c++) {
+            REQUIRE(m[i][c] == (i == c ? 1.0 : 0.0));
+        }
+    }
 
-	m.fill(4);
-	for(int i = 0; i < 6; i++){
-		for(int c = 0; c < 6; c++){
-			REQUIRE(m[i][c] == 4);
-		}
-	}
+    m.fill(4);
+    for (int i = 0; i < 6; i++) {
+        for (int c = 0; c < 6; c++) {
+            REQUIRE(m[i][c] == 4);
+        }
+    }
 }
 
-TEST_CASE("Multiplication"){
-	auto m = matrix<double>::identity(6);
-	auto r = m * 3;
+TEST_CASE("Multiplication") {
+    auto m = matrix<double>::identity(6);
+    auto r = m * 3;
 
-	for(int i = 0; i < 6; i++){
-		for(int c = 0; c < 6; c++){
-			REQUIRE(r[i][c] == (i == c? 3.0: 0.0));
-		}
-	}
+    for (int i = 0; i < 6; i++) {
+        for (int c = 0; c < 6; c++) {
+            REQUIRE(r[i][c] == (i == c ? 3.0 : 0.0));
+        }
+    }
 
-	m = matrix<double>(2,1);
-	r = matrix<double>(1,2);
-	m[1][0] = 1;
-	r[0][0] = 2;
-	r[0][1] = 3;
-	auto p = m * r;
-	REQUIRE(p.cols() == 2);
-	REQUIRE(p.rows() == 2);
-	REQUIRE(p[0][0] == 0);
-	REQUIRE(p[0][1] == 0);
-	REQUIRE(p[1][0] == 2);
-	REQUIRE(p[1][1] == 3);
+    m = matrix<double>(2, 1);
+    r = matrix<double>(1, 2);
+    m[1][0] = 1;
+    r[0][0] = 2;
+    r[0][1] = 3;
+    auto p = m * r;
+    REQUIRE(p.cols() == 2);
+    REQUIRE(p.rows() == 2);
+    REQUIRE(p[0][0] == 0);
+    REQUIRE(p[0][1] == 0);
+    REQUIRE(p[1][0] == 2);
+    REQUIRE(p[1][1] == 3);
 
-	bool except = false;
-	try{
-		matrix<double>::identity(7) * m;
-	}
-	catch(matrix_exception &e){
-		except = true;
-	}
-	REQUIRE(except);
+    bool except = false;
+    try {
+        matrix<double>::identity(7) * m;
+    } catch (matrix_exception &e) {
+        except = true;
+    }
+    REQUIRE(except);
 
-	m = matrix<double>::identity(6);
-	m *= 3;
-	for(int i = 0; i < 6; i++){
-		for(int c = 0; c < 6; c++){
-			REQUIRE(m[i][c] == (i == c? 3.0: 0.0));
-		}
-	}
+    m = matrix<double>::identity(6);
+    m *= 3;
+    for (int i = 0; i < 6; i++) {
+        for (int c = 0; c < 6; c++) {
+            REQUIRE(m[i][c] == (i == c ? 3.0 : 0.0));
+        }
+    }
 
-	m = matrix<double>(2,1);
-	r = matrix<double>(1,2);
-	m[1][0] = 1;
-	r[0][0] = 2;
-	r[0][1] = 3;
-	m *= r;
-	REQUIRE(m.cols() == 2);
-	REQUIRE(m.rows() == 2);
-	REQUIRE(m[0][0] == 0);
-	REQUIRE(m[0][1] == 0);
-	REQUIRE(m[1][0] == 2);
-	REQUIRE(m[1][1] == 3);
-
+    m = matrix<double>(2, 1);
+    r = matrix<double>(1, 2);
+    m[1][0] = 1;
+    r[0][0] = 2;
+    r[0][1] = 3;
+    m *= r;
+    REQUIRE(m.cols() == 2);
+    REQUIRE(m.rows() == 2);
+    REQUIRE(m[0][0] == 0);
+    REQUIRE(m[0][1] == 0);
+    REQUIRE(m[1][0] == 2);
+    REQUIRE(m[1][1] == 3);
 }
 
-TEST_CASE("Addition"){
-	auto m = matrix<double>::identity(3);
-	auto l = matrix<double>::identity(3);
-	auto r = m + l;
+TEST_CASE("Addition") {
+    auto m = matrix<double>::identity(3);
+    auto l = matrix<double>::identity(3);
+    auto r = m + l;
 
-	REQUIRE(r.cols() == 3);
-	REQUIRE(r.rows() == 3);
-	for(int i = 0; i < 3; i++){
-		for(int c = 0; c < 3; c++){
-			REQUIRE(r[i][c] == (i == c? 2.0: 0.0));
-		}
-	}
+    REQUIRE(r.cols() == 3);
+    REQUIRE(r.rows() == 3);
+    for (int i = 0; i < 3; i++) {
+        for (int c = 0; c < 3; c++) {
+            REQUIRE(r[i][c] == (i == c ? 2.0 : 0.0));
+        }
+    }
 
-	std::vector<double> row(3, 9.0);
-	l.add_row(row);
-	bool except = false;
-	try{
-		r = l + m;
-	}
-	catch(matrix_exception &e){
-		except = true;
-	}
-	REQUIRE(except);
+    std::vector<double> row(3, 9.0);
+    l.add_row(row);
+    bool except = false;
+    try {
+        r = l + m;
+    } catch (matrix_exception &e) {
+        except = true;
+    }
+    REQUIRE(except);
 
-	m += r;
-	for(int i = 0; i < 3; i++){
-		for(int c = 0; c < 3; c++){
-			REQUIRE(m[i][c] == (i == c? 3.0: 0.0));
-		}
-	}
-
+    m += r;
+    for (int i = 0; i < 3; i++) {
+        for (int c = 0; c < 3; c++) {
+            REQUIRE(m[i][c] == (i == c ? 3.0 : 0.0));
+        }
+    }
 }
 
-TEST_CASE("Subtraction"){
-	auto m = matrix<double>::identity(3);
-	auto l = matrix<double>::identity(3);
-	auto r = m - l;
+TEST_CASE("Subtraction") {
+    auto m = matrix<double>::identity(3);
+    auto l = matrix<double>::identity(3);
+    auto r = m - l;
 
-	REQUIRE(r.cols() == 3);
-	REQUIRE(r.rows() == 3);
-	for(int i = 0; i < 3; i++){
-		for(int c = 0; c < 3; c++){
-			REQUIRE(r[i][c] == 0.0);
-		}
-	}
+    REQUIRE(r.cols() == 3);
+    REQUIRE(r.rows() == 3);
+    for (int i = 0; i < 3; i++) {
+        for (int c = 0; c < 3; c++) {
+            REQUIRE(r[i][c] == 0.0);
+        }
+    }
 
-	std::vector<double> row(3, 9.0);
-	l.add_row(row);
-	bool except = false;
-	try{
-		r = l - m;
-	}
-	catch(matrix_exception &e){
-		except = true;
-	}
-	REQUIRE(except);
+    std::vector<double> row(3, 9.0);
+    l.add_row(row);
+    bool except = false;
+    try {
+        r = l - m;
+    } catch (matrix_exception &e) {
+        except = true;
+    }
+    REQUIRE(except);
 
-	r = matrix<double>(3, 3);
-	r.fill(2);
-	m -= r;
-	for(int i = 0; i < 3; i++){
-		for(int c = 0; c < 3; c++){
-			REQUIRE(m[i][c] == (i == c? -1: -2));
-		}
-	}
+    r = matrix<double>(3, 3);
+    r.fill(2);
+    m -= r;
+    for (int i = 0; i < 3; i++) {
+        for (int c = 0; c < 3; c++) {
+            REQUIRE(m[i][c] == (i == c ? -1 : -2));
+        }
+    }
 }
 
-TEST_CASE("Transpose"){
-	matrix<double> m(2,1);
-	m[0][0] = 1;
-	m[1][0] = 0;
-	const auto t = m.transpose();
-	REQUIRE(t.rows() == 1);
-	REQUIRE(t.cols() == 2);
-	REQUIRE(t[0][1] == 0);
-	REQUIRE(t[0][0] == 1);
+TEST_CASE("Transpose") {
+    matrix<double> m(2, 1);
+    m[0][0] = 1;
+    m[1][0] = 0;
+    const auto t = m.transpose();
+    REQUIRE(t.rows() == 1);
+    REQUIRE(t.cols() == 2);
+    REQUIRE(t[0][1] == 0);
+    REQUIRE(t[0][0] == 1);
 }
 
-TEST_CASE("Determinant"){
-	matrix<double> m(2,2);
-	m[0][0] = 1;
-	m[0][1] = 2;
-	m[1][0] = 1;
-	m[1][1] = 5;
-	// ab-dc
-	REQUIRE(m.determinant() == 3);
+TEST_CASE("Determinant") {
+    matrix<double> m(2, 2);
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 1;
+    m[1][1] = 5;
+    // ab-dc
+    REQUIRE(m.determinant() == 3);
 
-	matrix<double> r(4, 3);
-	r.fill(2);
-	bool except = false;
-	try{
-		r.determinant();
-	}
-	catch (matrix_exception &e){
-		except = true;
-	}
-	REQUIRE(except);
+    matrix<double> r(4, 3);
+    r.fill(2);
+    bool except = false;
+    try {
+        r.determinant();
+    } catch (matrix_exception &e) {
+        except = true;
+    }
+    REQUIRE(except);
 
-	std::vector<double> a = { 6, 1, 1 };
-	std::vector<double> b = { 4, -2,5 };
-	std::vector<double> c = { 2, 8, 7 };
-	std::vector<std::vector<double>> vec = {a, b, c};
+    std::vector<double> a = {6, 1, 1};
+    std::vector<double> b = {4, -2, 5};
+    std::vector<double> c = {2, 8, 7};
+    std::vector<std::vector<double>> vec = {a, b, c};
 
-	matrix<double> d(vec);
-	REQUIRE(d.determinant() == -306.0);
+    matrix<double> d(vec);
+    REQUIRE(d.determinant() == -306.0);
 }
 
-TEST_CASE("Inverse"){
-	std::vector<std::vector<double>> vec = {
-		{ 3, 3.5 }, 
-		{ 3.2, 3.6 }};
-	matrix<double> m(vec);
-	auto im = m.inverse();
-	REQUIRE(im[0][0] <= -8.9);
-	REQUIRE(im[0][0] >= -9.0);
-	REQUIRE(im[0][1] >= 8.65);
-	REQUIRE(im[0][1] <= 8.85);
-	REQUIRE(im[1][0] <= 8.1);
-	REQUIRE(im[1][0] >= 7.9);
-	REQUIRE(im[1][1] >= -7.6);
-	REQUIRE(im[1][1] <= -7.4);
+TEST_CASE("Inverse") {
+    std::vector<std::vector<double>> vec = {{3, 3.5}, {3.2, 3.6}};
+    matrix<double> m(vec);
+    auto im = m.inverse();
+    REQUIRE(im[0][0] <= -8.9);
+    REQUIRE(im[0][0] >= -9.0);
+    REQUIRE(im[0][1] >= 8.65);
+    REQUIRE(im[0][1] <= 8.85);
+    REQUIRE(im[1][0] <= 8.1);
+    REQUIRE(im[1][0] >= 7.9);
+    REQUIRE(im[1][1] >= -7.6);
+    REQUIRE(im[1][1] <= -7.4);
 
-	bool except = false;
-	m = matrix<double>(2, 2);
-	m[0][0] = 2;
-	m[1][1] = 2;
-	m[0][1] = 1;
-	m[1][0] = 4;
-	try{
-		m.inverse();
-	}
-	catch(matrix_exception &e){
-		except = true;
-	}
-	REQUIRE(except);
-	
+    bool except = false;
+    m = matrix<double>(2, 2);
+    m[0][0] = 2;
+    m[1][1] = 2;
+    m[0][1] = 1;
+    m[1][0] = 4;
+    try {
+        m.inverse();
+    } catch (matrix_exception &e) {
+        except = true;
+    }
+    REQUIRE(except);
 }
 
-TEST_CASE("Equality"){
-	auto m = matrix<double>::identity(2);
-	REQUIRE(m == matrix<double>::identity(2));
-	m[0][0] = 7;
-	REQUIRE(m != matrix<double>::identity(2));
-	std::vector<double> v = { 5 , 6 };
-	m.add_row(v);
-	REQUIRE(m != matrix<double>::identity(2));
+TEST_CASE("Equality") {
+    auto m = matrix<double>::identity(2);
+    REQUIRE(m == matrix<double>::identity(2));
+    m[0][0] = 7;
+    REQUIRE(m != matrix<double>::identity(2));
+    std::vector<double> v = {5, 6};
+    m.add_row(v);
+    REQUIRE(m != matrix<double>::identity(2));
 }
 
-TEST_CASE("Add Row"){
-	auto m = matrix<double>::identity(2);
-	bool except = false;
-	std::vector<double> v = { 1, 2 };
-	try{
-		m.add_row(v);
-		REQUIRE(m.rows() == 3);
-		REQUIRE(m.cols() == 2);
-		m.add_col(v);
-	}
-	catch(matrix_exception &e){
-		except = true;
-	}
-	REQUIRE(except);
+TEST_CASE("Add Row") {
+    auto m = matrix<double>::identity(2);
+    bool except = false;
+    std::vector<double> v = {1, 2};
+    try {
+        m.add_row(v);
+        REQUIRE(m.rows() == 3);
+        REQUIRE(m.cols() == 2);
+        m.add_col(v);
+    } catch (matrix_exception &e) {
+        except = true;
+    }
+    REQUIRE(except);
 }
-
